ef28e8ad9d89f6568688f56e5d02f17012406e7e
DevLinh <devlinh99qb@gmail.com>
2018-11-06 11:13:50 +0700

    Updates

---
+++ precache-manifest.35388ad8108fab69732646e1fda32a73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d2ba6fbf81ca12e5729",
+    "url": "/simplereactapp/static/js/runtime~main.4d2ba6fb.js"
+  },
+  {
+    "revision": "26714bd02a59e26feec0",
+    "url": "/simplereactapp/static/js/main.26714bd0.chunk.js"
+  },
+  {
+    "revision": "bcefb647ab5acc322d1b",
+    "url": "/simplereactapp/static/js/1.bcefb647.chunk.js"
+  },
+  {
+    "revision": "26714bd02a59e26feec0",
+    "url": "/simplereactapp/static/css/main.8c4f7f62.chunk.css"
+  },
+  {
+    "revision": "59194051ce8d1bdc73ae9f2e48ca599b",
+    "url": "/simplereactapp/index.html"
+  }
+];
